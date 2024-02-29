c87ab2b3b9777b93d3c5fe79bdc8120930813467
Kardenvan <glushkovlad98@gmail.com>
2024-02-29 11:06:52 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:personal_page/my_personal_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black26),
+        useMaterial3: true,
+      ),
+      home: const MyPersonalPage(),
+    );
+  }
+}
---
+++ lib/my_personal_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyPersonalPage extends StatefulWidget {
+  const MyPersonalPage({super.key});
+
+  @override
+  State<MyPersonalPage> createState() => _MyPersonalPageState();
+}
+
+class _MyPersonalPageState extends State<MyPersonalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+      ),
+    );
+  }
+}
